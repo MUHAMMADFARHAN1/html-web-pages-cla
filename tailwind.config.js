e94b2b594515ecf2f07067c19a2605db0c7f5dfe
Muhammad Farhan <farhan_75230@hotmail.com>
2025-04-13 20:03:03 +0200

    tailwind html egenrated and js file configured.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        back: "#f4f4f4",
+        box: "#ffffff",
+        shadow: "rgb(0 0 0 / 0.2)",
+        border: "#f5f5dc",
+        foot: "#23155b",
+      },
+    },
+  },
+  plugins: [],
+};
